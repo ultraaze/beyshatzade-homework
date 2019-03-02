84a247b8f36e6768d44d23a6b4691207a7afba2d
ultraaze <35341305+ultraaze@users.noreply.github.com>
2019-03-02 11:59:49 +0300

    Доработка
    в

--- Task2,3/src/Main.java
+++ Task2,3/src/Main.java
@@ -2,9 +2,7 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-
         int Arr[] = new int [args.length];
-
         for (int i= 0; i<args.length; i++){
             Arr[i] = Integer.valueOf(args [i]);
         }
@@ -15,27 +13,22 @@ public class Main {
         for (int j: Arr){
             Sum+=j;
         }
-
         System.out.println("Сумма элементов массива = " + Sum);
-
-
         for (int z: Arr) {
             if (z>1000){
-                System.out.println("Есть число больше 1000");
+                System.out.println("Массив - массивный");
                 break;
             }
         }
 // Здесь начинается 3 задача
-
         int Max = Arr[0];
         for (int x: Arr) {
-            if (x == 100500) continue;
-            else if (Max<x) Max = x;
+            if (x == 100500)
+                continue;
+            else if (Max<x)
+                Max = x;
         }
 
         System.out.println("Максимальное число массива = " + Max);
-
-
-
     }
 }
