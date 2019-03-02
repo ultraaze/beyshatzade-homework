f10c6a083c1569995e01e3196dc75d31ba7cfdd1
ultraaze <35341305+ultraaze@users.noreply.github.com>
2019-03-02 12:13:44 +0300

    task
    внешний вид

--- Task1/src/Main.java
+++ Task1/src/Main.java
@@ -5,12 +5,20 @@ public class Main {
         int a=Integer.valueOf(args[0]);
         int b=Integer.valueOf (args[1]);
 
-        if ((a<0)||(b<0))  System.out.println("Есть отрицательное число");
-        if (b!=0) System.out.println("Результат деления " + (float)a/b);
-        if (a>b) System.out.println("Первое число больше второго");
-        else if (a<b) System.out.println("Второе число больше первого");
-        else System.out.println("Числа равны");
-        if((a%2)==0) System.out.println("Первое число \"" +a+ "\" - четное");
+        if ((a<0)||(b<0))
+            System.out.println("Есть отрицательное число");
+        if (b!=0)
+            System.out.println("Результат деления " + (float)a/b);
+
+        if (a>b)
+            System.out.println("Первое число больше второго");
+        else if (a<b)
+            System.out.println("Второе число больше первого");
+        else
+            System.out.println("Числа равны");
+
+        if((a%2)==0)
+            System.out.println("Первое число \"" +a+ "\" - четное");
 
 
     }
