1c9329749d465ba2dfbb08c0f904b8eabe7c3cbc
ultraaze <35341305+ultraaze@users.noreply.github.com>
2019-03-04 12:11:26 +0300

    4 задание загрузка
    Скопировал папку

---
+++ Task4/src/Main.java
@@ -0,0 +1,21 @@
+import ru.mera.beyshatzade.postman.*;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        MailBox.AddEmail("Саша","Митя","эл1","1992");
+        MailBox.AddEmail("Петя","Лена","эл2","1921");
+        MailBox.AddEmail("Аня","Соловей","эл3","1999");
+        MailBox.PrintEmail();
+
+        MailBox.AddLetter("Саша","Вася","Обычное1","600232", "600239");
+        MailBox.AddLetter("Оля","Лена","Обычное2","600232", "600232");
+        MailBox.PrintLetter();
+
+
+
+    }
+}
---
+++ Task4/src/ru/mera/beyshatzade/postman/MailBox.java
@@ -0,0 +1,39 @@
+package ru.mera.beyshatzade.postman;
+import java.util.ArrayList;
+
+public class MailBox {
+    static private ArrayList EmailArr = new ArrayList();
+    static private ArrayList LetterArr = new ArrayList();
+
+    static public void AddEmail(String from, String to, String message_body, String receive_date){
+        Email New = new Email(from, to, message_body, receive_date);
+        EmailArr.add("from: " + New.from() + "; to: " + New.to() + "; body: "+ New.getmessage_body() +
+               "; date: " +  New.receive_date());
+    }
+
+
+    static public void PrintEmail(){
+        System.out.println("Все электронные письма: ");
+        for (int i=0; i<EmailArr.size(); i++ ) {
+            System.out.println((i+1)+": " + EmailArr.get(i));
+        }
+    }
+
+
+    static public void AddLetter(String from, String to, String message_body, String fromPostalIndex,
+                                 String toPostalIndex){
+        Letter New = new Letter(from, to, message_body, fromPostalIndex, toPostalIndex);
+        LetterArr.add("from: " + New.from() + "; to: " + New.to() + "; body: "+ New.getmessage_body() +
+                "; from-index: " +  New.fromPostalIndex() + "; to-index: " +  New.toPostalIndex());
+    }
+
+
+    static public void PrintLetter(){
+        System.out.println("Все обычные письма: ");
+        for (int i=0; i<LetterArr.size(); i++ ) {
+            System.out.println((i+1)+": " + LetterArr.get(i));
+        }
+    }
+
+
+}
