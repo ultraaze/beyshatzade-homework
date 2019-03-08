89773282e6420695d8a0bfa8dfee7ed62f16739e
ultraaze <35341305+ultraaze@users.noreply.github.com>
2019-03-08 11:13:51 +0300

    исправления
    конечные исправления

--- Task_4/src/ru/mera/Main.java
+++ Task_4/src/ru/mera/Main.java
@@ -2,12 +2,13 @@ import ru.mera.beyshatzade.postman.*;
 
 public class Main {
     public static void main(String[] args) {
-        MailBox.AddEmail("Саша","Митя","эл1","1992");
-        MailBox.AddEmail("Петя","Лена","эл2","1921");
-        MailBox.AddEmail("Аня","Соловей","эл3","1999");
-        MailBox.PrintEmail();
-        MailBox.AddLetter("Саша","Вася","Обычное1","600232", "60039");
-        MailBox.AddLetter("Оля","Лена","Обычное2","600232", "600232");
-        MailBox.PrintLetter();
+        MailBox Test = new MailBox();
+        Test.AddEmail("Саша","Митя","эл1","1992");
+        Test.AddEmail("Петя","Лена","эл2","1921");
+        Test.AddEmail("Аня","Соловей","эл3","1999");
+        Test.PrintEmail();
+        Test.AddLetter("Саша","Вася","Обычное1","600232", "60039");
+        Test.AddLetter("Оля","Лена","Обычное2","600232", "600232");
+        Test.PrintLetter();
     }
 }
--- Task_4/src/ru/mera/beyshatzade/postman/MailBox.java
+++ Task_4/src/ru/mera/beyshatzade/postman/MailBox.java
@@ -2,26 +2,26 @@ package ru.mera.beyshatzade.postman;
 import java.util.ArrayList;
 
 public class MailBox {
-    static private ArrayList EmailArr = new ArrayList();
-    static private ArrayList LetterArr = new ArrayList();
-    static public void AddEmail(String from, String to, String message_body, String receive_date){
+    private ArrayList EmailArr = new ArrayList();
+    private ArrayList LetterArr = new ArrayList();
+    public void AddEmail(String from, String to, String message_body, String receive_date){
         Email New = new Email(from, to, message_body, receive_date);
         EmailArr.add("from: " + New.getfrom() + "; to: " + New.getto() + "; body: "+ New.getmessage_body() +
                 "; date: " +  New.getreceive_date());
     }
-    static public void PrintEmail(){
+    public void PrintEmail(){
         System.out.println("Все электронные письма: ");
         for (int i=0; i<EmailArr.size(); i++ ) {
             System.out.println((i+1)+": " + EmailArr.get(i));
         }
     }
-    static public void AddLetter(String from, String to, String message_body, String fromPostalIndex,
+    public void AddLetter(String from, String to, String message_body, String fromPostalIndex,
                                  String toPostalIndex){
         Letter New = new Letter(from, to, message_body, fromPostalIndex, toPostalIndex);
         LetterArr.add("from: " + New.getfrom() + "; to: " + New.getto() + "; body: "+ New.getmessage_body() +
                 "; from-index: " +  New.getfromPostalIndex() + "; to-index: " +  New.gettoPostalIndex());
     }
-    static public void PrintLetter(){
+    public void PrintLetter(){
         System.out.println("Все обычные письма: ");
         for (int i=0; i<LetterArr.size(); i++ ) {
             System.out.println((i+1)+": " + LetterArr.get(i));
